0d0df9b45026cb8b82c0b98e1236698e0f19fc0c
PoojithaKodali <72956653+PoojithaKodali@users.noreply.github.com>
2021-01-19 09:03:06 +0530

    Create Course Schedule.py

---
+++ Course Schedule.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, n: int, a: List[List[int]]) -> bool:
+        novisit = 0
+        visit = 1
+        visited = 2
+        
+        def dfs(course):
+            if state[course] == visit:
+                return True
+            if state[course] == visited:
+                return False
+            state[course] = visit
+            
+            for j in g[course]:
+                if dfs(j):
+                    return True
+            state[course] = visited
+            return False
+        g = collections.defaultdict(list)
+        
+        for k, v in a:
+            g[k].append(v)
+            
+        state = [novisit for i in range(n)]
+        
+        return all(dfs(i)==False for i in range(n))
